f6b71144952f8d4b1c844627c4c0bca7cfcca677
Natawut Nupairoj <>
2021-09-30 21:12:50 +0700

    Version 0.1 - Simple Simpy

---
+++ 1-simple-example.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# First Simpy Example
+# for 2110636 Performance Evaluation and Analysis Class
+# Natawut Nupairoj, Chulalongkorn University, Thailand
+import simpy
+from simpy.util import start_delayed
+
+
+def simple_process(env, name, waitfor=50):
+    print('{:4d}:{} - begin process'.format(env.now, name))
+    yield env.timeout(waitfor)
+    print('{:4d}:{} - end process'.format(env.now, name))
+
+env = simpy.Environment()
+proc = env.process(simple_process(env, 'P0'))
+proc2 = start_delayed(env, simple_process(env, 'P1', waitfor=35), 8)
+env.run()
+
